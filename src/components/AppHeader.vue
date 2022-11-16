c02fe6216ac4356cbb5f3e6d4a2ae5907801f0ad
Mattia-Fasulo <mattia.fasulo@gmail.com>
2022-11-16 16:02:29 +0100

    add scaffold

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <div class="container">
+
+            <div class="logo">
+                <h1>Boolflix</h1>
+            </div>
+            <div class="search-bar">
+                <input type="text">
+                <button type="button">Search</button>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .logo {
+        h1 {
+            color: red;
+        }
+    }
+}
+</style>
